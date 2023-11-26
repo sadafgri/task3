8b26c1e4a55732e9808700bfe46b6ec41b52025f
sadafgri <sadafgri696@gmail.com>
2023-11-26 11:49:28 +0330

    code html sabt mahsole jadid

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<header>
+<style>
+    body{
+        text-align: center;
+        background-color: aliceblue;
+    }
+</style>
+</header>
+<body>
+ <form action="insert.php">
+     <label>new product:</label>
+     <input type="text" name="mahsol">
+     <button>save</button>
+ </form>
+</body>
+</html>
